f046cc96cf6e23391c6c3ae93beddc4a50e93f09
GabCabe <gabbriellaguna@gmail.com>
2023-03-16 19:32:04 -0300

    novo usuario

---
+++ Domain/Usuario.cs
@@ -0,0 +1,15 @@
+namespace Domain
+{
+    public class Usuario
+    {
+        public string Nome{get;set;}
+        public int Idade{get;set;}
+        public datetime DataDeNascimento{get;set;}
+        public decimal cpf{get;set;}
+        public string email{get;set;}
+
+
+
+
+    }
+}
